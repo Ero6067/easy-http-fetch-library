5d5689138176c779804d18703ecd68c71e21338d
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-07-07 16:30:45 -0700

    delete meathod created

--- app.js
+++ app.js
@@ -12,12 +12,17 @@ http.get('https://jsonplaceholder.typicode.com/users')
     email: 'jd@gmail.com'
   }
 
-  //Create Post
+  //Create User
   http.post('https://jsonplaceholder.typicode.com/users', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-  //Create put
+  //Update User
   http.put('https://jsonplaceholder.typicode.com/users/2', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
+
+  //Delete User
+  http.delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
--- easyhttp.js
+++ easyhttp.js
@@ -53,4 +53,20 @@
       .catch(err => reject(err));
       });
     }
+
+    // Make a HTTP DELETE request
+
+    delete(url) {
+      return new Promise((resolve, reject) => {
+        fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(() => resolve('Resource Deleted..'))
+      .catch(err => reject(err));
+      });
+    }
  }
